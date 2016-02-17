d6f7c741104766a5534b28730243af1aed033674
Greg D Soertsz <gsoertsz@Gregs-MacBook-Pro.local>
2016-02-17 13:16:54 +1100

    ditto

--- src/main/java/org/distributedproficiency/dojo/SwaggerConfiguration.java
+++ src/main/java/org/distributedproficiency/dojo/SwaggerConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +17,7 @@ public class SwaggerConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(ApiInfo.DEFAULT)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
